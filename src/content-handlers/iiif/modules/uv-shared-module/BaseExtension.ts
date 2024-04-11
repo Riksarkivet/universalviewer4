75ebfa4a632605878f7cccdd8997fb46a79a4866
Anders Pettersson <landsfiskalen@gmail.com>
2024-04-11 18:18:05 +0200

    First version of transcribed text panel, and right container panel.

--- src/content-handlers/iiif/modules/uv-shared-module/BaseExtension.ts
+++ src/content-handlers/iiif/modules/uv-shared-module/BaseExtension.ts
@@ -325,6 +325,10 @@ export class BaseExtension<T extends BaseConfig> implements IExtension {
       this.resize();
     });
 
+    this.extensionHost.subscribe(IIIFEvents.CLOSE_TEXT_RIGHT_PANEL, () => {
+      this.resize();
+    });
+
     this.extensionHost.subscribe(
       IIIFEvents.COLLECTION_INDEX_CHANGE,
       (collectionIndex: number) => {
@@ -1076,8 +1080,16 @@ export class BaseExtension<T extends BaseConfig> implements IExtension {
     return false;
   }
 
+  isRightContainerPanelEnabled(): boolean {
+    return Bools.getBool(this.data.config!.options.rightContainerPanelEnabled, true);
+  }
+
   isRightPanelEnabled(): boolean {
     return Bools.getBool(this.data.config!.options.rightPanelEnabled, true);
+  }
+
+  isTextRightPanelEnabled(): boolean {
+    return Bools.getBool(this.data.config!.options.textRightPanelEnabled, true);
   }
 
   isFooterPanelEnabled(): boolean {
