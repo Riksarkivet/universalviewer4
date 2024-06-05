00ed385e2dd07febd60c239d41ac740592a30007
Fei Lidén <fei.liden@riksarkivet.se>
2024-06-05 13:17:07 +0200

    Task #18468 - Create search input field for search left panel

--- src/content-handlers/iiif/BaseConfig.ts
+++ src/content-handlers/iiif/BaseConfig.ts
@@ -212,6 +212,9 @@ type SearchLeftPanelContent = DialogueContent &
     page: string;
     rangeHeader: string;
     title: string;
+    enterKeyword: string;
+    searchWithin: string;
+    clearSearch: string;
   };
 
 export type SearchLeftPanel = ModuleConfig & {
