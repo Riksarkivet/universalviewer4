a0ba8f5983ccf85b6603edf06540c68d4e33c78e
Anna Södergren <121164838+maranso@users.noreply.github.com>
2024-06-18 13:47:35 +0200

    Hide search and transcribed text panels if no searchservice in manifest

--- src/content-handlers/iiif/modules/uv-shared-module/BaseExtension.ts
+++ src/content-handlers/iiif/modules/uv-shared-module/BaseExtension.ts
@@ -1085,7 +1085,15 @@ export class BaseExtension<T extends BaseConfig> implements IExtension {
   }
 
   isSearchLeftPanelEnabled(): boolean {
-    return Bools.getBool(this.data.config!.options.searchLeftPanelEnabled, true);
+    if (!Bools.getBool(this.data.config!.options.searchLeftPanelEnabled, false)) {
+      return false;
+    }
+
+    if (!this.helper.getSearchService()) {
+      return false;
+    }
+
+    return true;
   }
 
   isRightContainerPanelEnabled(): boolean {
@@ -1097,7 +1105,15 @@ export class BaseExtension<T extends BaseConfig> implements IExtension {
   }
 
   isTextRightPanelEnabled(): boolean {
-    return Bools.getBool(this.data.config!.options.textRightPanelEnabled, true);
+    if (!Bools.getBool(this.data.config!.options.textRightPanelEnabled, false)) {
+      return false;
+    }
+
+    if (!this.helper.getSearchService()) {
+      return false;
+    }
+
+    return true;
   }
 
   isFooterPanelEnabled(): boolean {
