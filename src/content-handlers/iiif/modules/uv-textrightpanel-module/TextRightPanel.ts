2b0d4b72991d6ec2d093a6d1217e0f2842e2b3d0
Fei Lidén <fei.liden@riksarkivet.se>
2025-03-31 13:53:41 +0200

    #20475 - Refined: TextRightPanel will open when Alto XML files are available for volume. Added option for setting to keep panel closed on createI(). After create() the user triggers close/expand of panel in UI.

--- src/content-handlers/iiif/modules/uv-textrightpanel-module/TextRightPanel.ts
+++ src/content-handlers/iiif/modules/uv-textrightpanel-module/TextRightPanel.ts
@@ -4,7 +4,7 @@ import { TextRightPanel as TextRightPanelConfig } from "../../BaseConfig";
 import { Events } from "../../../../Events";
 import OpenSeadragonExtension from "../../extensions/uv-openseadragon-extension/Extension";
 import OpenSeadragon from "openseadragon";
-import { Clipboard } from "@edsilv/utils";
+import { Bools, Clipboard } from "@edsilv/utils";
 import { IExternalImageResourceData } from "manifesto.js";
 import { OpenSeadragonCenterPanel } from "../../modules/uv-openseadragoncenterpanel-module/OpenSeadragonCenterPanel";
 import { Shell } from "../uv-shared-module/Shell";
@@ -33,8 +33,17 @@ export class TextRightPanel extends RightPanel<TextRightPanelConfig> {
 
   create(): void {
     this.setConfig("textRightPanel");
-
+    
     super.create();
+
+    const shouldOpenPanel: boolean = Bools.getBool(
+      this.extension.getSettings().textRightPanelOpen,
+      this.options.panelOpen
+    );
+
+    if (shouldOpenPanel) {
+      this.toggle(true);
+    }
 
     if (
       this.config.options.copyToClipboardEnabled &&
@@ -422,10 +431,6 @@ export class TextRightPanel extends RightPanel<TextRightPanelConfig> {
         );
       }
       if (lines.length > 0) {
-        // Ensure the right text panel is expanded.
-        if (!this.isExpanded) {
-          this.toggle(true); // Expand the panel if it is not open already.
-        }
         this.$transcribedText.append(lines);
         this.$transcribedText.attr("data-text", this.clipboardText);
       } else {
